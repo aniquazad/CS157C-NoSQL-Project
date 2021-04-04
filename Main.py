5c783cedb7b80fa4e5acad944efdb9a9374e66a9
aniquazad <aniqua.azad@gmail.com>
2021-04-03 21:02:04 -0700

    added a few more use cases

---
+++ Main.py
@@ -0,0 +1,93 @@
+from PopulateDB import addArticlesDB, addBooksDB
+import UseCases
+from pynytimes import NYTAPI
+from datetime import datetime, date
+from pymongo import MongoClient, errors
+import sys
+import json
+import requests
+nyt = NYTAPI("qsPCmSV09wV4AbCCaJmXFPxo3nCwGtbU")
+myclient = MongoClient("mongodb://localhost:27017/")
+db = myclient["nyt"]
+article = db["article"]
+book = db["book"]
+LIMIT = int(input('Limit output to __ documents:\t'))
+
+def main_menu():
+    options = """
+    **************************************************************************
+    1. Add article
+    2. Get articles given a keyword value and rank >= number
+    3. Find articles with a word count >= number
+    4. Get the total word count of all articles by subsection name given a section
+    5. Read the abstracts based on a given keyword value
+    6. Find abstracts that contain an expression
+    7. Find other articles written by a person
+    8. Find articles that have multimedia and are a certain type of material
+    9. Get metadata of an article given an URL
+    10. Get popular sections and subsections
+    11. Update the document type for 1 article
+    12. Update the read status for an article
+    13. Add comment(s) to an article
+    14. Delete 1 article given the section name
+    15. Delete all articles with a certain keyword value and rank
+    16. Delete 1 article where the word count < number
+    **************************************************************************"""
+    print(options)
+    selected = str(input("Select an operation:\t"))
+    # LIMIT = int(input('Limit output to __ documents:\t'))
+    if selected == '1':
+        article_doc = UseCases.addArticle()
+        db.article.insert_one(article_doc)
+        print(article_doc)
+    elif selected == '2':
+        query = UseCases.findArticlesWKeyValueRank()
+        results = db.article.find({'keywords': {'$elemMatch':query}}).limit(LIMIT)
+        print_results(results)
+    elif selected == '3':
+        wc = UseCases.findArticlesNWordCount()
+        results = db.article.find({'word_count': {'$gte':wc}}, 
+            {'web_url':1, 'word_count': 1, 'abstract':1}).limit(LIMIT)
+        print_results(results)
+    elif selected == '4':
+        query = UseCases.getTotalWordCountSubsectionName()
+        query.append({'$limit':LIMIT})
+        results = db.article.aggregate(query)
+        print_results(results)
+    elif selected == '5':
+        query = UseCases.readAbstractBasedOnKeywordValue()
+        results = db.article.find(query, {'abstract':1,'web_url':1,'keywords':1}).limit(LIMIT)
+        print_results(results)
+    elif selected == '10':
+        query = UseCases.getPopularSections()
+        query.append({'$limit':LIMIT})
+        results = db.article.aggregate(query)
+        print_results(results)
+    elif selected == '12':
+        web_url = input('URL of the article you read:\t')
+        query = UseCases.updateReadStatusForArticle()
+        message = db.article.update_one({'web_url':web_url},query)
+        print("Updated: " + str(message.acknowledged))
+    elif selected == '13':
+        web_url = input('URL of the existing article you would like to add comments to:\t')
+        query = UseCases.addCommentsToArticle()
+        message = db.article.update_one({'web_url':web_url},query)
+        print("Updated: " + str(message.acknowledged))
+    else:
+        print("Good-bye!")
+        myclient.close() #close connection
+        exit()
+    main_menu()
+def print_results(results):
+    for r in results:
+        print(r)
+    
+if __name__ == "__main__": 
+    db_size_bytes = db.command("dbstats")['storageSize']
+    if db_size_bytes < 1000000000:
+        article.delete_many({})
+        #book.delete_many({})
+        addArticlesDB(myclient, db, article, nyt)
+    #addBooksDB(myclient, db, book, nyt)
+    print("This application displays articles from 2007-2020(inclusive)")
+    main_menu()
