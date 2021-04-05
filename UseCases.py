2d926c69f1884b19ca3d5e852433082c032db8e3
aniquazad <aniqua.azad@gmail.com>
2021-04-04 20:56:16 -0700

    Added more use cases

--- UseCases.py
+++ UseCases.py
@@ -67,7 +67,7 @@ def addArticle():
     print("\n[College Football, Media, World Business, Middle East, Pro Basketball, Music, Art & Design, Asia Pacific, Americas, Europe, Hockey, Bridge, Africa, Asia, Australia, Televsion]")
     subsection_name = input("Subsection name:\t")
     
-    print("\n[Metro, Sports, Letters, Business, National, Foreign, Editorial, Culture, ContinuousNews, OpEd, Summary, Science, New York, Finance, Magazin, Real Estate, Education, Dining]")
+    print("\n[Metro, Sports, Letters, Business, National, Foreign, Editorial, Culture, ContinuousNews, OpEd, Summary, Science, New York, Finance, Magazine, Real Estate, Education, Dining]")
     news_desk = input('News desk:\t')
 
     source = input("Source:\t")
@@ -77,7 +77,7 @@ def addArticle():
     'print_headline': '', 'name': None, 'seo': None, 'sub': None},'keywords':keywords,
     'pub_date':pub_date,'document_type':'article','news_desk':news_desk,
     'section_name':section_name,'subsection_name': subsection_name, 'byline':byline,'type_of_material':type_of_material,
-    '_id':article_id,'word_count':word_count,'uri':article_id}
+    '_id':article_id,'word_count':word_count,'uri':article_id, 'read_count':0}
     return article_doc
 ######################RETRIEVE: findArticlesWKeyValueRank()######################
 def findArticlesWKeyValueRank():
@@ -110,21 +110,19 @@ def readAbstractBasedOnKeywordValue():
 ######################RETRIEVE: getTypeOfMaterialAndMultimedia()######################
 
 ######################RETRIEVE: getInformationOfArticle()######################
-######################RETRIEVE: getPopularSections()######################
-def getPopularSections():
+
+######################RETRIEVE: getLongestSections()######################
+def getLongestSections():
     query = [
-        {'$group': {'_id': {'section': '$section_name', 'subsection':'$subsection_name'}, 'total':{'$sum' :1}}},
-        {'$project':{'section': '$_id.section', 'subsection': '$_id.subsection', 'articles' : '$total', '_id' : 0}},
-        {'$sort':{'articles': -1}}
-    ]
+        {'$group': {'_id': {'section': '$section_name', 'subsection':'$subsection_name'}, 'longest':{'$max': '$word_count'}}},
+        {'$project':{'section': '$_id.section', 'subsection': '$_id.subsection', 'words' : '$longest', '_id' : 0}},
+        {'$sort':{'words': -1}}]
     return query
 
-######################UPDATE: updateDocTypeOneArticle()######################
-
-######################UPDATE: updateReadStatusForArticle()######################
-def updateReadStatusForArticle():
-    query = {'$set':{'read':True}}
-    return query
+######################UPDATE: updateReadCountForArticle()######################
+def updateReadCountForArticle():
+    inc_read = {'$inc':{'read_count':1}}
+    return inc_read
 ######################UPDATE: addCommentsToArticle()######################
 def addCommentsToArticle():
     num_comments = int(input('How many comments will you add:\t'))
@@ -137,6 +135,41 @@ def addCommentsToArticle():
         comments_list.append(comment_info)
     query = {'$set':{'comments': comments_list}}
     return query
-
+######################DELETE: deleteManyArticlesWithSectionKeywordVal()######################
+def deleteManyArticlesWithSectionKeywordVal():
+    print('Sections: [Fashion, Parenting, Video, Travel, New York, Sports, Opinion, Business Day, Technology, Science, World, U.S., Arts, Opinion, World, Books, Homepage, College, Movies, Education, Health, Theater, Food]')
+    section_name = input("Delete articles with section name:\t")
+    kw_value = input("with keyword value:\t")
+    query = {'section_name':section_name,'keywords': {'$elemMatch':{'value':kw_value}}}
+    return query
+######################DELETE: deleteArticleWordReadCount()######################
+def deleteArticleWordReadCount():
+    wc = int(input('\tDelete article where word count is less than (integer):\t'))
+    rc = int(input('\tDelete article where read count is less than (integer):\t'))
+    query = {'word_count': {'$lt': wc}, 'read_count': {'$lt': rc}}
+    return query
+######################UPDATE: addKeywordArticle()######################
+def addKeywordArticle():
+    new_kw = {}
+    new_kw['name'] = input('Name of keyword [persons, subject, organizations]:\t')
+    new_kw['value'] = input('Keyword value:\t')
+    new_kw['rank'] = int(input('Rank of keyword(integer):\t'))
+    query = {'$push': {'keywords': new_kw}}
+    return query
+######################RETRIEVE: getNMostPopularKeywords()######################
+def getNMostPopularKeywords():
+    top_n_kw = int(input('Get top __ keywords for articles:\t'))
+    query = {'keywords.value': {'$slice':top_n_kw}, 'web_url':1,'headline.main':1,'_id':0}
+    return query
+######################RETRIEVE: getArticlesInSections()######################
+def getArticlesInSections():
+    print('Sections: [Fashion, Parenting, Video, Travel, New York, Sports, Opinion, Business Day, Technology, Science, World, U.S., Arts, Opinion, World, Books, Homepage, College, Movies, Education, Health, Theater, Food]')
+    section_choices = []
+    choice = ""
+    print("Enter the sections as they appear. Press \'d\' when done.")
+    while choice != "d":
+        choice = input("\tSection:\t")
+        section_choices.append(choice)
+    return section_choices[:-1]
 if __name__ == "__main__": 
     print(__name__)
